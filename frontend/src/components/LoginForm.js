64b7c389368d5690f1a3afb3eaee5c5fc9ea3247
Ozioma Michael <okonisaozioma@gmail.com>
2024-05-31 09:48:57 +0100

    Implemented Users name on Dashboard

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,30 +1,50 @@
 // frontend/src/components/LoginForm.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { login } from '../services/api';
-import './LoginForm.css';
+import { useUser } from '../context/UserContext';
+import { login } from '../services/api'; 
+import './Login.css'
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { setUser } = useUser(); // Get setUser function from UserContext
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await login({ email, password });
-      navigate('/dashboard');
+      const userData = await login({ email, password });
+      setUser(userData); // Set user data in context
+      navigate('/dashboard'); // Redirect to dashboard
     } catch (error) {
-      console.error(error);
+      setError(error.response.data.message);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-      <button type="submit">Login</button>
-    </form>
+    <div>
+      <h2>Login</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 };
 
